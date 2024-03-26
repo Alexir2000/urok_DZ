627d2e9383c73f633342737597178d20622e8944
Alexir2000 <hit2007@list.ru>
2024-03-26 15:08:19 +0300

    изменение3 в main тест отмены

--- name.py
+++ name.py
@@ -1,14 +1,3 @@
+a, b = 10 , 108
 
-print(" Конфиктная запись в main")
-
-for i in range(8)
-    Print (i)
-b = 10
-
-if a > b:
-    print(" a больше чем b")
-elif b > a:
-    print(" b больше чем a")
-else:
-    print(" b равно a")
 
